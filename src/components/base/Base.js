7d0bd70fa7135132b9a5cad1496b994fad26faa9
alimov <alikce.2001@gmail.com>
2021-05-29 17:53:22 +0300

    dfsd

--- src/components/base/Base.js
+++ src/components/base/Base.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 import './Base.css'
 import Search from './search/Search'
 import Register from './register/Register'
 import Profile from './profile/Profile'
+
 const Base = () => {
     return <div>
         <div className='baseBox'>
             <div className='maryamBox'>
-                <h2>Maryam</h2>
+                <h2><Link className='maryamLink' to={'/'}>Maryam</Link></h2>
             </div>
             <Search className='search'/>
             {localStorage.getItem('token') ? <Profile/> : <Register/>} 
