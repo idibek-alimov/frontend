5bfe5ab30fa1d4fd2e432a69fe0c7a4410e5150f
alimov <alikce.2001@gmail.com>
2021-05-30 00:12:39 +0300

    dsf

--- src/components/base/search/Search.js
+++ src/components/base/search/Search.js
@@ -1,20 +1,32 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import './Search.css'
 import {store}from '../../../store/store'
 import {refreshTheSearch} from '../../../store/actions'
 import {Redirect,useHistory} from 'react-router-dom'
+import axios from 'axios'
 const Search = () => {
     let search='';
+    let categoryID = 0;
     let history = useHistory();
+    const [category,setCategory] = useState()
+    
+    useEffect(()=>{
+        axios.get('https://maryam-backend.herokuapp.com/subcategory')
+             .then(res=>setCategory(res.data.results))
+             .catch(err=>console.log(err))
+    },[])
+
+    
     const onInputChange = (evt) => {
         search= evt.target.value
-    
-
+    }
+    const onCategoryHandle = (evt) => {
+        categoryID = evt.target.value 
     }
    
     const onFormSubmit = (evt) => {
         evt.preventDefault()
-        store.dispatch(refreshTheSearch(0,search));
+        store.dispatch(refreshTheSearch(categoryID,search));
         history.push('/search')
         window.location.reload()
         
@@ -22,7 +34,15 @@ const Search = () => {
     return <div>
         <div className='searchBox'>
             <form className='searchBox' onSubmit={onFormSubmit}> 
-                <button className='all'>All</button>
+                <select  className='selectCategory'  onChange={onCategoryHandle}>
+                    <option value={0}>all</option>
+                    {category ? category.map(category=>
+                        <option value={category.id} key={category.id}>
+    	                   {category.name}
+                        </option>
+                    ) : ''}
+              
+                </select>
                 <input className='searchInput' onChange={onInputChange} />
                 <button className='searchSubmit' type='submit' >Submit</button>
             </form>
